618fd062dbc7b65037b9c389116ee332316c2229
Marcin Banik <mrn.banik@gmail.com>
2019-07-30 10:59:01 +0200

    Add filter to front

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const News = require("../models/news");
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
- res.render('news', { title: 'News' });
+  const search = req.query.search || "";
+  const findNews = News
+    .find({ title: new RegExp(search.trim(), "i") })
+    .sort({ created: -1 });
+  findNews.exec((err, data) => {
+    if (err) return console.log("Error: ", err)
+    res.render('news', { title: 'News', data, search });
+  });
 });
 
 module.exports = router;
