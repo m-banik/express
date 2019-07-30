7e2288e0b3c691dd7dd2be6d8dde436d6644ed15
Marcin Banik <mrn.banik@gmail.com>
2019-07-30 19:42:47 +0200

    Update api.js file - resolve query issue

--- routes/api.js
+++ routes/api.js
@@ -1,31 +1,28 @@
 const express = require('express');
 const News = require("../models/news");
 const router = express.Router();
-const defaultSort = -1;
 
 router.get('/', (req, res) => {
- const search = req.query.search || "";
- let sort = req.query.sort || defaultSort;
- if (sort !== -1 || sort !== 1) {
-  sort = defaultSort;
- };
- const findNews = News
-  .find({ title: new RegExp(search.trim(), "i") })
-  .sort({ created: sort })
-  .select("_id title description");
- findNews.exec((err, data) => {
-  if (err) return console.log("Error: ", err)
-  res.json(data);
- });
+  const search = req.query.search || "";
+  let sort = Number(req.query.sort);
+  if (sort !== -1 && sort !== 1) sort = -1;
+  const findNews = News
+    .find({ title: new RegExp(search.trim(), "i") })
+    .sort({ created: sort })
+    .select("_id title description");
+  findNews.exec((err, data) => {
+    if (err) return console.log("Error: ", err)
+    res.json(data);
+  });
 });
 
 router.get('/:id', (req, res) => {
- const id = req.params.id;
- const findNews = News.findById(id).select("_id title description");
- findNews.exec((err, data) => {
-  if (err) return console.log("Error: ", err)
-  res.json(data);
- });
+  const id = req.params.id;
+  const findNews = News.findById(id).select("_id title description");
+  findNews.exec((err, data) => {
+    if (err) return console.log("Error: ", err)
+    res.json(data);
+  });
 });
 
 module.exports = router;
