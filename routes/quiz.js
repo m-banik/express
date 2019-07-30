400e711b619ebee5f9797396af125eaf7ae0d302
Marcin Banik <mrn.banik@gmail.com>
2019-07-30 14:49:57 +0200

    Add quiz form

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const Quiz = require("../models/quiz");
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
- res.render('quiz', { title: 'Quiz' });
+router.get('/', (req, res) => {
+ Quiz.find({}, (err, data) => {
+  if (err) return console.log("Error: ", err);
+  const show = !req.session.vote;
+  let sum = 0;
+  data.forEach(item => sum += item.vote);
+  res.render('quiz', { title: 'Quiz', data, show, sum });
+ });
+});
+
+router.post('/', (req, res) => {
+ const id = req.body.quiz;
+ Quiz.findOne({ _id: id }, (err, data) => {
+  data.vote++;
+  data.save(err => {
+   if (err) return console.log("Error: ", err);
+   req.session.vote = 1;
+   res.redirect("/quiz");
+  });
+ });
 });
 
 module.exports = router;
