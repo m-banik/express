8b911ee6c3219c9244ca28f2b0c9f4139b59ce35
Marcin Banik <mrn.banik@gmail.com>
2020-04-06 11:32:08 +0200

    Packages update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
- db: '',
+ db: 'mongodb+srv://admin:3iMpKWgtqTfFrkMC@cluster0-za3ff.mongodb.net/test?retryWrites=true&w=majority',
  keySession: ["yourKey"],
  maxAgeSession: 24 * 60 * 60 * 1000
 };
